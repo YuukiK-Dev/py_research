d27695cb7fa6c356c7ea6fdbcb03da035cb734cc
Yuuki Kobayashi <inarote1204@gmail.com>
2026-04-11 16:09:53 +0900

    入口画面整理

--- supporter_app/app.py
+++ supporter_app/app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-
+st.set_page_config(page_title="支援者アプリ",layout="centered")
 st.markdown(
         """
         <style>
@@ -15,10 +15,12 @@ st.markdown(
 
 
 
-st.write("has connections:", "connections" in st.secrets)
-st.write("has gsheets:", "connections" in st.secrets and "gsheets" in st.secrets["connections"])
+# st.write("has connections:", "connections" in st.secrets)
+# st.write("has gsheets:", "connections" in st.secrets and "gsheets" in st.secrets["connections"])
 
-st.set_page_config(page_title="支援者アプリ",layout="centered")
 
 st.title("支援者アプリ(試作)")
 st.write("左のメニューからページを選んでください")
+
+if st.button("入力を開始する"):
+    st.switch_page("page/2_支援者入力.py")
